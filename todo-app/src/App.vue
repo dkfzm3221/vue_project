967ec17ca48b02f80357598b35c2a6e5952abdf9
kimjaeseop <dkfzm3221@naver.com>
2020-11-13 11:01:04 +0900

    first commit

---
+++ todo-app/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+  <TodoHeader/>
+  <TodoInput @sendTodo="parentSendTodo"/>
+  <TodoList :todoList="todoList"/>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+
+
+
+export default {
+  name: 'App',
+  components: {TodoHeader, TodoInput, TodoList},
+  data() {
+    return {
+      id: 0,
+      todoList: []
+    }
+  },
+  methods:{
+    parentSendTodo(todo) {
+      var obj = {
+        id: ++this.id,
+        todo: todo
+      }
+      this.todoList.push(obj)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ todo-app/src/components/TodoList.vue
@@ -0,0 +1,42 @@
+<template>
+    <section>
+        <ul>
+            <li v-for="item in todoList" :key="item.id">
+                <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+                {{item.todo}}
+            </li>
+        </ul>
+    </section>
+</template>
+<script>
+export default {
+    props:['todoList'],
+}
+</script>
+<style scoped>
+ul{
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+}
+li{
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background-color: 5px;
+}
+.checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+}
+
+.removeBtn {
+    margin-left: auto;
+    color: #de4343;
+}
+</style>
